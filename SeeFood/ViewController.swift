18f3d86fb35bf8b8a22aed9f522a5515df47ccc6
kssmith9 <kssmith9@ncsu.edu>
2019-10-30 13:48:48 +0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Kevin Smith on 10/30/19.
+//  Copyright © 2019 Kevin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
